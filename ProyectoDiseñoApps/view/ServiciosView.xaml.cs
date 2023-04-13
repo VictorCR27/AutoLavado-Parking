639656864e24315ad2eb846a0765b7d87db267ff
VictorCR27 <danielsegurajaime@gmail.com>
2023-04-13 01:17:59 -0600

    Cambio ServiciosView

--- ProyectoDiseñoApps/view/ServiciosView.xaml.cs
+++ ProyectoDiseñoApps/view/ServiciosView.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserControl = System.Windows.Controls.UserControl;
 
 namespace ProyectoDiseñoApps.view
 {
@@ -20,9 +22,58 @@ namespace ProyectoDiseñoApps.view
     /// </summary>
     public partial class ServiciosView : UserControl
     {
+
         public ServiciosView()
         {
             InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Aquí puedes implementar el código para procesar y enviar los datos ingresados.
+            System.Windows.MessageBox.Show("Datos enviados");
+        }
+
+        private void BronceService_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BasicService_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PremiumService_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WashAndParkingService_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ParkingService_Checked(object sender, RoutedEventArgs e)
+        {
+            ParkingPlateTextBox.IsEnabled = ParkingService.IsChecked == true;
+            ParkingHourTextBox.IsEnabled = ParkingService.IsChecked == true;
+        }
+
+        private void ParkingService_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ParkingPlateTextBox.IsEnabled = false;
+            ParkingHourTextBox.IsEnabled = false;
+        }
+
+        private void VehicleModelTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void VehiclePlateTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
